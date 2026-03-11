cda1386bffb1a61f2775a1ae861adc2b5e92af76
2004mainaeric@gmail.com <2004mainaerick@gmail.com>
2026-03-11 10:25:35 +0300

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,3 @@
+document.getElementById("welcome").addEventListener("click", function () {
+  document.getElementById("message").innerHTML = "Welcome to Erim Technologies we are very lucky to have you as our customer,YOU CAN NAVIGATE TO THE PAGES ON TOP,WELCOME!😉🤗";
+});
